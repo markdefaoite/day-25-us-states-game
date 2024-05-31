8e66454dd86fd771ed9561e498c1c3226df7a6b9
Mark White <markdefaoite@hotmail.com>
2024-05-31 17:00:45 -0400

    states missed by player are output to csv

--- main.py
+++ main.py
@@ -16,18 +16,26 @@ state_list = state_data["state"].to_list()
 correct_guesses = []
 score = 0
 
-
 while score < 50:
     answer_state = screen.textinput(title=f"{score}/50 States guessed correctly", prompt="Guess a state").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in state_list:
+            if state not in correct_guesses:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
     if answer_state in state_list and answer_state not in correct_guesses:
         correct_guesses.append(answer_state)
         score += 1
         state = state_data[state_data.state == answer_state]
         x = int(int(state.x.iloc[0]))
         y = int(state.y.iloc[0])
-        pen.goto(x,y)
+        pen.goto(x, y)
         pen.write(answer_state)
-
 
 
 
